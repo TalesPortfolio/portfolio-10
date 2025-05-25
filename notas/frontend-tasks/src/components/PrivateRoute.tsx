3723cc1bcfdf9615e117cd1d483da6c7724ce39d
tales1982 <142450583+TalesPortfolio@users.noreply.github.com>
2025-05-25 21:28:09 +0200

    estado normal

--- notas/frontend-tasks/src/components/PrivateRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom'
-
-const PrivateRoute = () => {
-  const user = localStorage.getItem('user') // ou token
-
-  return user ? <Outlet /> : <Navigate to="/" />
-}
-
-export default PrivateRoute
